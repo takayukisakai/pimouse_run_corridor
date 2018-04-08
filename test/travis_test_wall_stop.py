0a48f3bfc9e9dbdc23dd011b32df6ecb4b11fc04
Takayuki Sakai <sakai.t.doshisha.mechanics@gmail.com>
2018-04-08 02:20:29 +0000

    test chmod

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -17,13 +17,13 @@ class WallStopTest(unittest.TestCase):
 		return left, right
 
 	def test_io(self):
-		left, right = self.set_and_get(400,100,100,0)
+		left, right = self.set_and_get(400,100,100,0) #total: 600
 		self.assertTrue(left == 0 and right == 0, "can't stop")
 
-		left, right = self.set_and_get(400,0,0,99)
+		left, right = self.set_and_get(400,0,0,99) #total: 499
 		self.assertTrue(left != 0 and right != 0, "can't move again")
 
-		left, right = self.set_and_get(150,0,200,150)
+		left, right = self.set_and_get(150,0,200,150) #total 500
 		self.assertTrue(left == 0 and right == 0, "can't stop")
 
 if __name__ == '__main__':
