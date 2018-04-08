17eafd62ae6abd04960a1097f9a1f2aa0b7d51ad
Takayuki Sakai <sakai.t.doshisha.mechanics@gmail.com>
2018-04-08 02:48:03 +0000

    add a run script

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -3,6 +3,6 @@
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 cd ~/catkin_ws/src
-git clone --depth=1 https://github.com/citueda/pimouse_ros.git
+git clone --depth=1 https://github.com/takayukisakai/pimouse_ros.git
 cd ~/catkin_ws
 catkin_make
