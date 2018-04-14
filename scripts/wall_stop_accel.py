a452b3ed5963edee880f871ff913e7e513750fd2
Takayuki Sakai <sakai.t.doshisha.mechanics@gmail.com>
2018-04-14 02:16:13 +0000

    accel2

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -18,17 +18,17 @@ class WallStop():
 		rate = rospy.Rate(10)
 		data = Twist()
 
-	accel = 0.02
-	data.linear.x= 0.0	
-	while not rospy.is_shutdown():
-		data.linear.x += accel	
+		accel = 0.02
+		data.linear.x= 0.0	
+		while not rospy.is_shutdown():
+			data.linear.x += accel	
 
-		if self.sensor_values.sum_all >= 50: data.linear.x = 0.0
-		elif data.linear.x <= 0.2:           data.linear.x = 0.2
-		elif data.linear.x >= 0.8:           data.linear.x = 0.8
+			if self.sensor_values.sum_all >= 50: data.linear.x = 0.0
+			elif data.linear.x <= 0.2:           data.linear.x = 0.2
+			elif data.linear.x >= 0.8:           data.linear.x = 0.8
 			
-		self.cmd_vel.publish(data)
-		rate.sleep()
+			self.cmd_vel.publish(data)
+			rate.sleep()
 
 if __name__ == '__main__':
 	rospy.init_node('wall_stop')
