3491c64c0291a22ec00880ce84459ed768e92c48
Takayuki Sakai <sakai.t.doshisha.mechanics@gmail.com>
2018-04-14 02:25:09 +0000

    accel3

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -20,7 +20,7 @@ class WallStopTest(unittest.TestCase):
 		left, right = self.set_and_get(400,100,100,0) #total: 600
 		self.assertTrue(left == right == 0, "can't stop")
 
-		left, right = self.set_and_get(40,0,0,99) #total: 49
+		left, right = self.set_and_get(40,0,0,9) #total: 49
 		self.assertTrue(0 < left == right < 1000, "can't move again")
 
 		time.sleep(5.0)
